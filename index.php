3292e595c37f2f9dc69159f6a2c4a62853d4d997
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-24 15:01:38 +0100

    Add: Function getPassword & input number

--- index.php
+++ index.php
@@ -6,6 +6,38 @@
   <title>PHP Strong Password Generator</title>
 </head>
 <body>
+
+<style>
+  :root {
+    font-family: Arial;
+  }
+</style>
+
+<form method="get">
+  <b>Seleziona il  n° di caratteri della Password</b>
+  <br>
+  <input type="number" id="length" name="length" min="5" max="20">
+  <input type="submit">
+</form>
+
+<?php
+
+$length = $_GET['length'];
+
+  function generatePassword($length) {
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_+=<>?';
+      $randomString = '';
+
+      for ($i = 0; $i < $length; $i++) {
+          $randomString .= $characters[rand(0, strlen($characters) - 1)];
+      }
+
+      return $randomString;
+  }
+
+  echo generatePassword($length);
+
+?>
   
 </body>
 </html>
